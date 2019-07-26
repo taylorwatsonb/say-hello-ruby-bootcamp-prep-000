eb9dbd17badfb05fe35f822da9d1c11fc54bfffb
Taylor Watson <taylorwatsonb@gmail.com>
2019-07-26 23:14:17 +0000

    Automatically backed up by Learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,5 +1,5 @@
 # Build your say_hello method here
 
-def greeting(name, language="Ruby")
+def say_hello(name, language="Ruby")
   puts "Hello, #{name}. We heard you are a great #{language} programmer."
 end
